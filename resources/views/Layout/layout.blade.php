022b04454a16a0ae1c3dcc93a9035d10a2db6b13
Nathan <Naatie.Odd@gmail.com>
2023-11-01 15:44:23 +0100

    Laravel added

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -1,30 +1,15 @@
 <!DOCTYPE html>
 <!doctype html>
-<html lang="nl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="MyFreeTime" description="De website waar je al je vrije tijd kan bijhouden."
+    <meta name="Webwarrior" description="The best web based text rpg"
     @vite(['resources/css/style.css'])
     <title>@yield('title')</title>
 </head>
 <body>
-<header>
-    <div class="head">
-        <div class="titelnav">
-        </div>
-        <div class="navbar">
-            <div class="link"><a href="/">Thuispagina</a></div>
-            <div id="zoekbalkheader"><input type="text" placeholder="Zoeken.."></div>
-            <div class="link"><a href="/nieuws">Nieuws</a></div>
-            <div class="link"><a href="/inloggen">Inloggen</a></div>
-        </div>
-    </div>
-</header>
-<footer></footer>
 </body>
-
-
 </html>
